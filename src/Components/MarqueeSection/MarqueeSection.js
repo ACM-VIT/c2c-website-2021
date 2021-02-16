d5f982ce589a4067838f07fb45dcb1aeeab88178
madrigal1 <kurupgokul11@gmail.com>
2021-02-16 16:49:13 +0530

    feat: start of marquee

---
+++ src/Components/MarqueeSection/MarqueeSection.js
@@ -0,0 +1,14 @@
+
+/** Styles */
+import './MarqueeSection.css';
+const MarqueeSection = ()=>{
+
+  return (
+    <div className="marqContainer">
+    <p>
+    	COMPUTING CHANGE CHALLENGING CREATIVITY
+    </p>
+    </div>
+ )
+};
+export default MarqueeSection;
