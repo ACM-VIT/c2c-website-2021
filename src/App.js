375e3bc98f9f6f2cffeb6e2760a362b99ccb7a6c
AritriBasu <AritriBasu@users.noreply.github.com>
2021-02-17 00:50:41 +0530

    added sponsor logos and structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import LandingSection from './Components/LandingSection/LandingSection';
+import SponsorsSection from './Components/SponsorsSection/SponsorsSection';
 
 function App() {
   return (
     <div className="landing">
       <LandingSection/>
+      <SponsorsSection/>
     </div>
   );
 }
