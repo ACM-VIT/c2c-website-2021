52dafa83faf795e42ae904dee3ebd2ce7009272a
rishabhkeshan <rishabh.keshan2019@vitstudent.ac.in>
2021-02-17 21:18:28 +0530

    added carousel

--- src/Components/CarouselSection/CarouselSection.js
+++ src/Components/CarouselSection/CarouselSection.js
@@ -1,16 +1,27 @@
 import image from "../../assets/Rectangle 5.svg";
 import "./CarouselSection.css";
-
+import register from "../../assets/register.svg";
+import Carousel, { autoplayPlugin, slidesToShowPlugin, Dots } from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
 const CarouselSection = () => {
   return (
     <article className="carouselsection">
       <div className="flex flex-col lg:flex-row items-center justify-center mx-auto py-28">
-        <div className="carousel-image">
-          <img style={{ display: "flex" }} src={image} alt="Carousel" />{" "}
+        <div className="carousel-image flex items-center justify-center">
+          <Carousel
+            infinite
+            animationSpeed={3000}
+            autoPlay={3000}
+            clickToChange
+            arrowLeft
+          >
+            <img src={image} alt="carousel-1" />
+            <img src={image} alt="carousel-1" />
+          </Carousel>
         </div>
 
-        <div className="flex flex-col">
-          <div className="carousel-text1 px-8">
+        <div className="flex flex-col items-start">
+          <div className="carousel-text1 lg:px-8">
             We're back with a bang,
             <br />
             but{" "}
@@ -18,7 +29,7 @@ const CarouselSection = () => {
               let's flex first!
             </span>
           </div>
-          <div className="lg:-ml-20">
+          <div className="lg:-ml-20 px-0 z-50">
             <div className="carousel-text2" style={{ background: "#6695D3" }}>
               5000+ participants
             </div>
