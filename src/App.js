48fdcf5f985e675e247638997196749254130e1d
rishabhkeshan <rishabh.keshan2019@vitstudent.ac.in>
2021-02-17 08:15:36 +0530

    added marquee

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import LandingSection from './Components/LandingSection/LandingSection';
+import MarqueeSection from './Components/MarqueeSection/MarqueeSection';
+import VideoSection from './Components/VideoSection/VideoSection';
 
 function App() {
   return (
     <div className="landing">
-      <LandingSection/>
+      <LandingSection />
+      <MarqueeSection />
+      <VideoSection />
     </div>
   );
 }
