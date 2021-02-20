3e32b3f359449f750c39bd19fa0806a250f324df
madrigal1 <kurupgokul11@gmail.com>
2021-02-20 17:26:33 +0530

    feat: oragnisers data

--- src/Components/OrganisersSection/oragnisersdata.js
+++ src/Components/OrganisersSection/oragnisersdata.js
@@ -1,57 +1,95 @@
-
-
+import sarthak from '../../assets/boardAndFaculty/Sarthak.jpg'
+import hrishita from '../../assets/boardAndFaculty/Hrishita.jpg'
+import eesha from '../../assets/boardAndFaculty/Eesha.png'
+import anjali from '../../assets/boardAndFaculty/Anjali.jpg'
+import kashish from '../../assets/boardAndFaculty/Kashish.jpg'
+import shovin from '../../assets/boardAndFaculty/Shovin.jpg'
+import dhruv from '../../assets/boardAndFaculty/Dhruv.jpg'
+import devansh from '../../assets/boardAndFaculty/Devansh.jpg'
+import iishi from '../../assets/boardAndFaculty/Iishi.jpg'
+import nimit from '../../assets/boardAndFaculty/Nimit.jpg'
+import jerelyn from '../../assets/boardAndFaculty/Jerelyn.jpg'
+import garima from '../../assets/boardAndFaculty/Garima.jpg'
+import elio from '../../assets/boardAndFaculty/Elio.png'
+import siddharth from '../../assets/boardAndFaculty/Siddharth.jpg'
+import sriya from '../../assets/boardAndFaculty/Sriya.jpg'
 
 const organisers  = [
-    {
-        pic:"https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=976&q=80",
-        name:"Kashish Mittal",
-        position:"Web Master",
-    },
-    {
-        pic:"https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=976&q=80",
-        name:"Kashish Mittal",
-        position:"Web Master",
-    },
-    {
-        pic:"https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=976&q=80",
-        name:"Kashish Mittal",
-        position:"Web Master",
-    },
-    {
-        pic:"https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=976&q=80",
-        name:"Kashish Mittal",
-        position:"Web Master",
-    },
-    {
-        pic:"https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=976&q=80",
-        name:"Kashish Mittal",
-        position:"Web Master",
-    },
-    {
-        pic:"https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=976&q=80",
-        name:"Kashish Mittal",
-        position:"Web Master",
-    },
-    {
-        pic:"https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=976&q=80",
-        name:"Kashish Mittal",
-        position:"Web Master",
-    },
-    {
-        pic:"https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=976&q=80",
-        name:"Kashish Mittal",
-        position:"Web Master",
-    },
-    {
-        pic:"https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=976&q=80",
-        name:"Kashish Mittal",
-        position:"Web Master",
-    },
-    {
-        pic:"https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=976&q=80",
-        name:"Kashish Mittal",
-        position:"Web Master",
-    },
+   {
+       pic: sarthak,
+       name:"Sarthak Gupta",
+       position:"President"
+   },
+   {
+      pic:hrishita,
+      name:"Hrishita",
+      position:"Managing Director", 
+   },
+   {
+       pic:eesha,
+       name:"Eesha",
+       position:"Technical Director"
+   },
+   {
+     pic:anjali,
+     name:"Anjali",
+     position:"General Secretary"
+   },
+   {
+     pic:kashish,
+     name:"Kashish",
+     position:"Web-Master"
+   },
+   {
+     pic:shovin,
+     name:"Shovin",
+     position:"UI/UX Lead"
+   },
+   {
+     pic: dhruv,
+     name:"Dhruv Roy",
+     position:"Treasurer"
+   },
+   {
+    pic:devansh,
+    name:"Devansh Mehta",
+    position:"App Lead",
+   },
+   {
+    pic:iishi,
+    name:"Iishi Patel",
+    position:"Research Lead",
+   },
+   {
+    pic:nimit,
+    name:"Nimit Jain",
+    position:"Competitive Lead",
+   },
+   {
+    pic:jerelyn,
+    name:"Jerelyn",
+    position:"ACM-W Lead"
+   },
+   {
+    pic:garima,
+    name:"Jerelyn Preeja",
+    position:"ACM-W Lead"
+   },
+   {
+    pic:elio,
+    name:"Elio Jordan Lopes",
+    position:"Web Projects Guide"
+   },
+   {
+    pic:siddharth,
+    name:"Siddharth Nahar",
+    position:"Competitive Guide"
+   },
+   {
+    pic:sriya,
+    name:"Sriya Reddi",
+    position:"Operations Head"
+   },
 ]
 
 export default organisers;
