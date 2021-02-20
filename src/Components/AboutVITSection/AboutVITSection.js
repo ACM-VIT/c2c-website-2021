917e23e3321581e04a93a5fd58ebe1508049e084
Malika01 <malika.chikara@gmail.com>
2021-02-20 19:53:41 +0530

    feature:about and footer section added

---
+++ src/Components/AboutVITSection/AboutVITSection.js
@@ -0,0 +1,29 @@
+import "./AboutVITSection.css";
+
+const AboutVITSection = () => {
+
+  return (
+      <article className="aboutSection">
+          <div>
+              <div className="aboutHead">
+                  About VIT
+              </div>
+              <p className="aboutContent">Vellore Institute of Technology, Vellore is one of the most revered and distinguished engineering institution which holds the rare distinction of being one of the few colleges to impart world-class education along with encouragement towards co-curricular activities which help in overall development and transform students into responsible adults with a strong ethical core.
+             Vellore Institute of Technology was the birth-child of the tireless and relentless perseverance of our respected Chancellor, Dr. G. Viswanathan, whose objective since the very inception of this institution has been to provide an environment that would help foster intellectual aptitude, enhance human perception and create a society of extra-ordinary professionals capable of thinking new, out-of-the-box ideas and having the skills to implement them, in order to help the entire society as a whole.</p>
+          </div>
+
+          <div>
+              <div className="aboutHead">
+                  About C2C
+              </div>
+              <p className="aboutContent">A thrilling tech sprint awaited by numerous innovators, Code2Create witnesses a plethora of skill sets. Code2Create is all about designing, building and creating; a place where graphic designers, software developers, app developers, and web developers collaborate intensively on projects.
+              Code2Create'19 saw more than 500 participants. The judges were renowned experts in their respective fields. Participants from across the country indulge in 36 hours of intense brainstorming, designing, creating and testing, along with some engaging and very enjoyable side quests.
+              Code2Create is ACM-VIT’s flagship event and one of the grandest annual events hosted in VIT.</p>
+          </div>
+
+      </article>
+  
+  );
+};
+
+export default AboutVITSection;
