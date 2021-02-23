847f32e93b1f37556fd34f951bd3bd15ff4a785a
rishabhkeshan <rishabh.keshan2019@vitstudent.ac.in>
2021-02-23 21:27:01 +0530

    tried parallax

--- src/Components/CarouselSection/CarouselSection.js
+++ src/Components/CarouselSection/CarouselSection.js
@@ -10,71 +10,111 @@ import image9 from "../../assets/carousel-images/img9.jpg";
 import image10 from "../../assets/carousel-images/img10.JPG";
 import image16 from "../../assets/carousel-images/img16.JPG";
 import image17 from "../../assets/carousel-images/img17.JPG";
+// import { Parallax } from 'react-scroll-parallax';
 
 import "./CarouselSection.css";
-import register from "../../assets/register.svg";
 import Carousel from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
+import { Parallax, ParallaxLayer } from "react-spring/renderprops-addons";
+import Plx from "react-plx";
+
+
+const parallaxData  = [
+  {
+    start: '.StickyText-trigger',
+    duration: '30vh',
+    properties: [
+      {
+        startValue: 0,
+        endValue: -50,
+        unit: 'vh',
+        property: 'translateY',
+      },
+      {
+        startValue: 0,
+        endValue: 1,
+        property: 'opacity',
+      },
+    ],
+  },
+  {
+    start: '.StickyText-trigger',
+    startOffset: '60vh',
+    duration: '30vh',
+    properties: [
+      {
+        startValue: -50,
+        endValue: -100,
+        unit: 'vh',
+        property: 'translateY',
+      },
+      {
+        startValue: 1,
+        endValue: 0,
+        property: 'opacity',
+      },
+    ],
+  },
+];
 const CarouselSection = () => {
 
   return (
-    <article className="carouselsection">
-      <div className="flex flex-col lg:flex-row items-center justify-center mx-auto mt-32 mb-0">
-        <div
-          className="carousel-image flex items-center justify-center z-20"
-        >
-          <Carousel
-            infinite
-            animationSpeed={1000}
-            autoPlay={4000}
-            clickToChange
-            arrowLeft={<button id="leftArr"></button>}
-            arrowRight={<button id="rightArr"></button>}
-            addArrowClickHandler
-          >
-            <img src={image1} alt="carousel-img-1" />
-            <img src={image2} alt="carousel-img-1" />
-            <img src={image3} alt="carousel-img-3" />
-            <img src={image4} alt="carousel-img-4" />
-            <img src={image5} alt="carousel-img-5" />
-            <img src={image6} alt="carousel-img-6" />
-            <img src={image7} alt="carousel-img-7" />
-            <img src={image8} alt="carousel-img-8" />
-            <img src={image9} alt="carousel-img-9" />
-            <img src={image10} alt="carousel-img-10" />
-            <img src={image16} alt="carousel-img-11" />
-            <img src={image17} alt="carousel-img-12" />
-            
-          </Carousel>
-        </div>
+    <Plx className="StickyText" parallaxData={parallaxData}>
+      <article className="carouselsection">
+        <div className="flex flex-col lg:flex-row items-center justify-center mx-auto mt-32 mb-0">
+          <div className="carousel-image flex items-center justify-center z-20">
+            <Carousel
+              infinite
+              animationSpeed={1000}
+              autoPlay={4000}
+              clickToChange
+              arrowLeft={<button id="leftArr"></button>}
+              arrowRight={<button id="rightArr"></button>}
+              addArrowClickHandler
+            >
+              <img src={image1} alt="carousel-img-1" />
+              <img src={image2} alt="carousel-img-1" />
+              <img src={image3} alt="carousel-img-3" />
+              <img src={image4} alt="carousel-img-4" />
+              <img src={image5} alt="carousel-img-5" />
+              <img src={image6} alt="carousel-img-6" />
+              <img src={image7} alt="carousel-img-7" />
+              <img src={image8} alt="carousel-img-8" />
+              <img src={image9} alt="carousel-img-9" />
+              <img src={image10} alt="carousel-img-10" />
+              <img src={image16} alt="carousel-img-11" />
+              <img src={image17} alt="carousel-img-12" />
+            </Carousel>
+          </div>
 
-        <div className="flex flex-col items-start">
-          <div className="carousel-text1 lg:px-8">
-            We're back with a bang,
-            <br />
-            but{" "}
-            <span style={{ fontWeight: "900", color: "#48BA86" }}>
-              let's flex first!
-            </span>
-          </div>
-          <div className="lg:-ml-20 z-50">
-            <div className="carousel-text2" style={{ background: "#6695D3" }}>
-              5000+ participants
+          <div className="flex flex-col items-start">
+            <div className="carousel-text1 lg:px-8">
+              We're back with a bang,
+              <br />
+              but{" "}
+              <span style={{ fontWeight: "900", color: "#48BA86" }}>
+                let's flex first!
+              </span>
             </div>
-            <div className="carousel-text2" style={{ background: "#DE7979" }}>
-              1000+ teams
-            </div>
-            <div className="carousel-text2" style={{ background: "#CC9463" }}>
-              10,000+ stickers distributed
-            </div>
-            <div className="carousel-text2" style={{ background: "#6695D3" }}>
-              25+ sponsors
+            <div className="lg:-ml-20 z-50">
+              <div className="carousel-text2" style={{ background: "#6695D3" }}>
+                5000+ participants
+              </div>
+              <div className="carousel-text2" style={{ background: "#DE7979" }}>
+                1000+ teams
+              </div>
+              <div className="carousel-text2" style={{ background: "#CC9463" }}>
+                10,000+ stickers distributed
+              </div>
+              <div className="carousel-text2" style={{ background: "#6695D3" }}>
+                25+ sponsors
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div id="sLink"></div>
-    </article>
+        <div id="sLink"></div>
+      </article>
+    </Plx>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ParallaxProvider } from "react-scroll-parallax";
 
 ReactDOM.render(
-  <>
+  <ParallaxProvider>
     <App />
-  </>,
-  document.getElementById('root')
+  </ParallaxProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
