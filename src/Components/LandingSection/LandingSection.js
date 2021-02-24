7221bc5f4c1a8ed42f9aebc4d3a166e04dcbbc27
rishabhkeshan <rishabh.keshan2019@vitstudent.ac.in>
2021-02-24 17:23:54 +0530

    added devfolio

--- src/Components/LandingSection/LandingSection.js
+++ src/Components/LandingSection/LandingSection.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { useState, useEffect, useRef } from "react";
 import { slide as Menu } from "react-burger-menu";
-
+import RBN from "react-burger-nav";
 import c2clogo from "../../assets/c2clogo.svg";
 import register from "../../assets/register.svg";
 import {Link} from 'react-router-dom'
@@ -38,58 +38,58 @@ const LandingSection = ({scrollToFAQ, scrollToOrganizers, scrollToSponsors})=>{
   return (
     <article className="landsection">
       <section className={header}>
-        <a
-          href="https://c2c.acmvit.in"
-          className="text-white"
-        >
+        <a href="https://c2c.acmvit.in" className="text-white">
           <img className="acmlogo header-image" src={c2clogo} alt="C2C Logo" />
         </a>
         <div className="navigate">
-          <Link to={"/about"}>
-            <a>
-              <div className="navigatetitle">About</div>
+          {/* <Menu right> */}
+            <Link to={"/about"}>
+              <a>
+                <div className="navigatetitle">About</div>
+              </a>
+            </Link>
+            <a onClick={() => scrollToSponsors()}>
+              <div className="navigatetitle">Sponsors</div>
             </a>
-          </Link>
-          <a onClick={() => scrollToSponsors()}>
-            <div className="navigatetitle">Sponsors</div>
-          </a>
-          <a onClick={() => scrollToOrganizers()}>
-            <div className="navigatetitle">Organisers</div>
-          </a>
+            <a onClick={() => scrollToOrganizers()}>
+              <div className="navigatetitle">Organisers</div>
+            </a>
 
-          <a onClick={() => scrollToFAQ()}>
-            <div className="navigatetitle">FAQ</div>
-          </a>
+            <a onClick={() => scrollToFAQ()}>
+              <div className="navigatetitle">FAQ</div>
+            </a>
+          {/* </Menu> */}
         </div>
-        <img className="registerbutton navreg" src={register} alt="Register Now" />
-      </section>
-        <section className="titlecontainer">
-          <h1 className="title">
-            <span className="greentext">The Hackathon </span>everyone’s been{" "}
-            <span className="greentext"> waiting for</span>
-          </h1>
-
-          <div className="subtitle">
-            Experience the fifth edition of Code2Create now, right from the
-            comfort of your home
-          </div>
-
-          {/* <a href="https://devfolio.co" target="_blank" rel="noreferrer noopener"> */}
-
-          <img className="registerbutton" src={register} alt="Register Now" />
-
-          <script
-            defer
-            async
-            src="https://apply.devfolio.co/v2/sdk.js"
-          ></script>
+        {/* <div
+          style={{ transform: "scale(0.6)" }}
+        >
           <div
             className="apply-button"
-            data-hackathon-slug="YOUR-HACKATHON-SLUG"
-            data-button-theme="light"
-            style={{ height: "44px", width: "312px" }}
+            data-hackathon-slug="acm-c2c"
+            data-button-theme="dark-inverted"
+            style={{ height: "18px", width: "110px", transform: "scale(0.4)" }}
           ></div>
-        </section>
+        </div> */}
+      </section>
+      <section className="titlecontainer flex flex-row items-center justify-center">
+        <h1 className="title">
+          <span className="greentext">The Hackathon </span>everyone’s been{" "}
+          <span className="greentext"> waiting for</span>
+        </h1>
+
+        <div className="subtitle">
+          Experience the fifth edition of Code2Create now, right from the
+          comfort of your home
+        </div>
+
+        {/* <a href="https://devfolio.co" target="_blank" rel="noreferrer noopener"> */}
+        <div
+          className="apply-button"
+          data-hackathon-slug="acm-c2c"
+          data-button-theme="dark-inverted"
+          style={{ height: "18px", width: "110px" }}
+        ></div>
+      </section>
     </article>
   );
 }
