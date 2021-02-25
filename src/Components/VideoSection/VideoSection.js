5f31ec73f1fc6760b879a088cd3bf7879f1c17c8
rishabhkeshan <rishabh.keshan2019@vitstudent.ac.in>
2021-02-25 12:50:58 +0530

    nav link on about pushed

--- src/Components/VideoSection/VideoSection.js
+++ src/Components/VideoSection/VideoSection.js
@@ -3,7 +3,6 @@ import Tilt from "react-tilt"
 /** Styles */
 import "./VideoSection.css";
 import c2c from "../../assets/c2caftermovie.mp4";
-import "plyr-react/dist/plyr.css";
 const VideoSection = () => {
 
   useEffect(() => {
@@ -25,26 +24,6 @@ const VideoSection = () => {
   return (
     <Tilt options={{ scale: 1, perspective: 1000, max: 15 }}>
       <article className="window__frame__container">
-        {/* <Tilt
-        className="videoplayer"
-        options={{ scale: 1, perspective: 1000, max: 15 }}
-      >
-        <div className="containerheader">
-          <div className="flex flex-row">
-            <div className="circle green"></div>
-            <div className="circle yellow"></div>
-            <div className="circle red"></div>
-          </div>
-          <div className="bar"></div>
-          <div>
-            <div className="circle"></div>
-          </div>
-        </div>
-        <div className="videocontainer container">
-          <video className="flex items-center justify-center" style={{borderRadius:"16px", overflow:"hidden" }} controls autostart="true" autoPlay loop={true} muted src={c2c} type="video/mp4" />
-        </div>
-      </Tilt> */}
-        {/* <Player /> */}
         <div
           className="window__frame__image"
           style={{ padding: "56.25% 0 0 0", position: "relative" }}
